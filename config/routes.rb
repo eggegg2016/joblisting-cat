8a8dc8f8482de7e260849813a770d4c589bed0fd
eggegg <eggegg2016@163.com>
2017-01-17 20:31:56 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
 
    namespace :admin do
    resources :jobs do
+    
    	 member do
         post :publish
         post :hide
       end
+
+     resources :resumes 
      end
    end
 
